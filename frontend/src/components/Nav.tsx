e7ae590b2321db633fb7e9f5e8901a613580093d
davidvchang <dvalenzuelachang@gmail.com>
2025-04-08 23:24:16 -0700

    Add: icons to Nav in NavBar

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { Link } from 'react-router-dom';
 
 interface PropsLinks {
     to: string,
+    icon: ReactNode
     text: string
 }
 
-const Nav:React.FC<PropsLinks> = ({to, text}) => {
+const Nav:React.FC<PropsLinks> = ({to, icon, text}) => {
   return (
-    <Link to={to} className='hover:text-slate-200 hover:transition duration-300'>{text}</Link>
+    <Link to={to} className='flex items-center gap-2 hover:text-slate-200 hover:transition duration-300'>
+      {icon}
+      {text}
+    </Link>
   )
 }
 
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HeartPulse } from 'lucide-react';
+import { HeartPulse, House, PawPrint, Users, User } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import Nav from './Nav';
 import Btn from './Btn';
@@ -7,15 +7,17 @@ import Btn from './Btn';
 const NavBar:React.FC = () => {
   return (
     <div className='flex w-full h-16 bg-teal-600 px-7 items-center justify-between'>
-        <div className='flex items-center gap-10'>
+        <div className='flex items-center gap-16'>
             <Link to='/admin' className='flex gap-2 items-center text-white'>
                 <HeartPulse className='w-8 h-8'/>
                 <span className='text-xl font-semibold'>PetCare Clinic</span>
             </Link>
 
-            <div className='flex gap-5 items-center text-white'>
-                <Nav to='/admin' text='Home'/>
-                <Nav to='/admin/profile' text='Profile'/>
+            <div className='flex gap-7 items-center text-white'>
+                <Nav to='/admin' text='Home' icon={<House className='w-5 h-5'/>}/>
+                <Nav to='/admin/pets' text='Pets' icon={<PawPrint className='w-5 h-5'/>}/>
+                <Nav to='/admin/clients' text='Clients' icon={<Users className='w-5 h-5'/>}/>
+                <Nav to='/admin/profile' text='Profile' icon={<User className='w-5 h-5'/>}/>
             </div>
 
         </div>
