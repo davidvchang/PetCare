8e54b7cc1c4c7122953d309d97ffed725a43a182
davidvchang <dvalenzuelachang@gmail.com>
2025-04-13 22:29:40 -0700

    Add: cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,18 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 import usersRoutes from './routes/users.routes.js'
+import petsRoutes from './routes/pets.routes.js'
 
 const app = express()
 
 app.set('port', process.env.PORT || 4000)
 
 app.use(morgan("dev"))
+app.use(cors("*"))
 app.use(express.json())
 
 app.use('/api/users', usersRoutes)
-app.use('/api/pets', usersRoutes)
+app.use('/api/pets', petsRoutes)
 
 export default app
